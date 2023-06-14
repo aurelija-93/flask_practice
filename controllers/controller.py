8de90cb221689a785dc505d385b1a316c40d2648
Aurelija Bellerby <aurelija.bellerby@gmail.com>
2023-06-14 14:11:01 +0100

    updated app, controller, models

---
+++ controllers/controller.py
@@ -0,0 +1,8 @@
+from flask import render_template, request, redirect
+from app import app
+from models.event_list import events
+from models.event import Event
+
+@app.route('/events')
+def index():
+    return render_template("index.html", title="Events", events=events)
---
+++ models/event_list.py
@@ -0,0 +1,22 @@
+from models.event import *
+import datetime
+
+event1 = Event(
+    datetime.date(2023, 6, 16),
+    "Board Game Friday",
+    20,
+    "Library",
+    "An evening of board game madness! Bring your own games or choose some from the library's stock!",
+    True
+    )
+
+event2 = Event(
+    datetime.date(2023, 6, 21),
+    "Summer Solstice 2023",
+    100,
+    "Campus Gardens",
+    "Celebrate midsummer the pagan way - an interactive day of games, performances, and much more!",
+    False
+)
+
+events = [event1, event2]
