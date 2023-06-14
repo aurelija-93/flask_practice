17736334057d5e14b6c5ce203b226e69693a6a04
Aurelija Bellerby <aurelija.bellerby@gmail.com>
2023-06-14 15:11:06 +0100

    event planner route working

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,6 +3,10 @@ from app import app
 from models.event_list import events
 from models.event import Event
 
-@app.route('/events')
+@app.route("/events")
 def index():
     return render_template("index.html", title="Events", events=events)
+
+@app.route("/planner")
+def planner():
+    return render_template("planner.html", title="Event Planner")
